23daa40e59eb9dcc857e46068957e668e1c7e74b
Hd-Lg <59037834+Hd-Lg@users.noreply.github.com>
2022-10-29 22:50:59 +0200

    Reorganize repos

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # business_dashboard
 A React Dashboard using SyncFusion components
+
+
+
+![Dashboard_Screenshot](./react-dashboard-screenshot1.png)
+![Dashboard_Screenshot2](./react-dashboard-screenshot2.png)
+![Dashboard_Screenshot3](./react-dashboard-screenshot3.png)
---
+++ src/components/index.jsx
@@ -0,0 +1,32 @@
+import Button from "./Button";
+import Cart from "./Cart";
+import Chat from "./Chat";
+import Footer from "./Footer";
+import Header from "./Header";
+import Navbar from "./Navbar";
+import Notification from "./Notification";
+import Sidebar from "./Sidebar";
+import ThemeSettings from "./ThemeSettings";
+import UserProfile from "./UserProfile";
+
+import LineChart from "./Charts/LineChart";
+import Pie from "./Charts/Pie";
+import SparkLine from "./Charts/SparkLine";
+import Stacked from "./Charts/Stacked";
+
+export {
+    Button,
+    Cart,
+    Chat,
+    Footer,
+    Header,
+    Navbar,
+    Notification,
+    Sidebar,
+    ThemeSettings,
+    UserProfile,
+    LineChart,
+    Pie,
+    SparkLine,
+    Stacked,
+};
